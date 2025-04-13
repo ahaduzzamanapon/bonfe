b77add18aa4172167720143d216632f17924f3ed
ahaduzzamanapon <ahaduzzamanapon@gmail.com>
2025-04-13 15:46:05 +0600

    login title

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -25,7 +25,11 @@
                                 <div class="row">
                                     <div class="col-md-12">
                                         <h2 class="text-center">
-                                            <span>SLOPE</span>
+                                            <?php
+                                            $setting = DB::table('sitesettings')->first();
+                                            ?>
+
+                                            <span>{{ !empty($setting) ? $setting->name : 'Title' }}</span>
                                         </h2>
                                     </div>
                                 </div>
